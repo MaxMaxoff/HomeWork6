599e525e543db5ba0287afa0f70bb7d224a55ad8
Maxim VT <maxim.vt@gmail.com>
2018-05-09 19:38:19 +0300

    Add project files.

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,465 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SupportLibrary;
+using System.IO;
+
+/// <summary>
+/// Максим Торопов
+/// Ярославль
+/// Домашняя работа 6 урока
+/// 
+/// Достаточно решить 3 задачи.
+/// Старайтесь разбивать программы на подпрограммы.
+/// Переписывайте в начало программы условие и свою фамилию.
+/// Все программы сделать в одном решении.
+/// </summary>
+namespace HomeWork6
+{
+    /// <summary>
+    /// Описываем делегат.
+    /// В делегате описывается сигнатура методов, на которые сможет ссылаться делегат в дальнейшем (хранить в себе)
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>    
+    public delegate double Fun(double x, double y);
+
+    /// <summary>
+    /// Делегат для функции с одним параметром
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public delegate double Func(double x);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                SupportMethods.PrepareConsoleForHomeTask("1 - Task 1\n" +
+                  "2 - Task 2\n" +
+                  "3 - Task 3\n" +
+                  "4 - Task 4\n" +
+                  "5 - Task 5\n" +
+                  "6 - Task 6\n" +
+                  "0 (Esc) - Exit\n");
+                ConsoleKeyInfo key = Console.ReadKey();
+                Console.WriteLine();
+                switch (key.Key)
+                {
+                    case ConsoleKey.D1:
+                        Task1();
+                        break;
+                    case ConsoleKey.D2:
+                        Task2();
+                        break;
+                    case ConsoleKey.D3:
+                        Task3();
+                        break;
+                    case ConsoleKey.D4:
+                        Task4();
+                        break;
+                    case ConsoleKey.D5:
+                        Task5();
+                        break;
+                    case ConsoleKey.D6:
+                        Task6();
+                        break;
+                    case ConsoleKey.D0:
+                    case ConsoleKey.Escape:
+                        return;
+                    default:
+                        break;
+                }
+            } while (true);
+        }
+
+        /// <summary>
+        /// Создаем метод, который принимает делегат.
+        /// Т.е. на практике, этот метод сможет принимать любой метод с такой же сигнатурой как у делегата
+        /// </summary>
+        /// <param name="F"></param>
+        /// <param name="x"></param>
+        /// <param name="b"></param>
+        public static void Table(Fun F, double x, double y, double b)
+        {
+            double y1 = 0D;
+            Console.WriteLine("A \tX \tF(x, a)");
+            while (x <= b)
+            {
+                y1 = y;
+                while (y1 <= b)
+                {
+                    Console.WriteLine($"{y1:0.000} \t{x:0.000} \t{F(x, y1):0.000}");
+                    y1 += 2;
+                }
+                x += 2;
+            }
+            Console.WriteLine("---------------------");
+        }
+
+        /// <summary>
+        /// Создаем метод для передачи его в качестве параметра в Table
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static double MyFunc1(double x, double y)
+        {
+            return y * x * x;
+        }
+
+        /// <summary>
+        /// Создаем метод для передачи его в качестве параметра в Table
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static double MyFunc2(double x, double y)
+        {
+            return y * Math.Sin(x);
+        }
+
+        /// <summary>
+        /// Задача 1.
+        /// 
+        /// Изменить программу вывода функции так, чтобы можно было передавать функции типа double(double,double).
+        /// Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).
+        /// </summary>
+        static void Task1()
+        {
+            //Prepare console
+            SupportMethods.PrepareConsoleForHomeTask("Изменить программу вывода функции так, чтобы можно было передавать функции типа double(double,double).\n" +
+                "Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).");
+
+            
+
+            // Создаем новый делегат и передаем ссылку на него в метод Table
+            Console.WriteLine("Таблица функции MyFunc:");
+
+            // Параметры метода и тип возвращаемого значения, должны совпадать с делегатом
+            //Table(new Fun(MyFunc1), -2, 2);
+            //Console.WriteLine("Еще раз та же таблица, но вызов организован по новому");
+
+            // Упрощение(c C# 2.0).Делегат создается автоматически.
+            Console.WriteLine("Таблица функции a * x^2:");
+            Table(MyFunc1, -2, -2, 2);
+
+            Console.WriteLine("Таблица функции a * sin(x):");
+            Table(MyFunc2, -2, -2, 2); // Можно передавать уже созданные методы
+            
+            // Упрощение(с C# 2.0). Использование анонимного метода
+            //Table(delegate (double x) { return x * x; }, 0, 3);
+
+            SupportMethods.Pause();
+
+        }
+
+        /// <summary>
+        /// Method function F1
+        /// return x ^ 2 - 50 * x + 10;
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns> double F</returns>
+        public static double F1(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+
+        /// <summary>
+        /// Method function F2
+        /// x ^ 2 + 20 * x + 5;
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns> double F</returns>
+        public static double F2(double x)
+        {
+            return x * x + 20 * x + 5;
+        }
+
+        /// <summary>
+        /// Method function F3 x ^ 2
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns> double F</returns>
+        public static double F3(double x)
+        {
+            return x * x ;
+        }
+
+        /// <summary>
+        /// Method function F4 sin(x)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns> double F</returns>
+        public static double F4(double x)
+        {
+            return Math.Sin(x);
+        }
+
+        /// <summary>
+        /// Method function F5 x ^ 3
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns> double F</returns>
+        public static double F5(double x)
+        {
+            return x * x * x;
+        }
+
+        /// <summary>
+        /// Method save to file
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="h"></param>
+        public static void SaveFunc(Func F, string fileName, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(F(x));
+                x += h;// x=x+h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+
+        /// <summary>
+        /// Method load from file
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static double Load(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            return min;
+        }
+
+        /// <summary>
+        /// Задача 2.
+        /// 
+        /// *Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата.
+        /// Сделать меню с различными функциями и представьте пользователю выбор для какой функции и на каком отрезке находить минимум.
+        /// Используйте массив делегатов.
+        /// </summary>
+        static void Task2()
+        {
+            // Создание и инициализация массива делегатов
+            Func[] functions = new Func[5];
+
+            // Заполнение массива делегатов
+            functions[0] = F1;
+            functions[1] = F2;
+            functions[2] = F3;
+            functions[3] = F4;
+            functions[4] = F5;
+
+            // Меню выбора функций
+            do
+            {
+                SupportMethods.PrepareConsoleForHomeTask("*Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата.\n" +
+                "Сделать меню с различными функциями и представьте пользователю выбор для какой функции и на каком отрезке находить минимум.\n" +
+                "Используйте массив делегатов.\n" +
+                "1 - функция: x ^ 2 - 50 * x + 10\n" +
+                "2 - функция: x ^ 2 + 20 * x + 5\n" +
+                "3 - функция: x ^ 2\n" +
+                "4 - функция: sin(x)\n" +
+                "5 - функция: x ^ 3\n" +
+                "0 (Esc) - Exit\n");
+                ConsoleKeyInfo key = Console.ReadKey();
+                Console.WriteLine();                
+                                
+                switch (key.Key)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.D2:
+                    case ConsoleKey.D3:
+                    case ConsoleKey.D4:
+                    case ConsoleKey.D5:
+                        SaveFunc(functions[int.Parse(key.KeyChar.ToString()) - 1],
+                            "data.bin",
+                            SupportMethods.RequestIntValue("Введите начало отрезка, например -100: "),
+                            SupportMethods.RequestIntValue("Введите конец отрезка, например 100: "),
+                            SupportMethods.RequestDoubleValue("Введите точность, например 0,5: "));
+
+                        SupportMethods.Pause($"Минимум функции на заданном отрезке с заданной точностью равен {Load("data.bin"):F2}");
+                        break;
+                    case ConsoleKey.D0:
+                    case ConsoleKey.Escape:
+                        return;
+                    default:
+                        break;
+                }
+            } while (true);
+
+
+        }
+
+        /// <summary>
+        /// Задача 3.
+        /// 
+        /// Подсчитать количество студентов:
+        /// а) учащихся на 5 и 6 курсах;
+        /// б) подсчитать сколько студентов в возрасте от 18 до 20 лет на каком курсе учатся(частотный массив);
+        /// в) отсортировать список по возрасту студента;
+        /// г) *отсортировать список по курсу и возрасту студента.
+        /// </summary>
+        static void Task3()
+        {
+            //Создаем список студентов
+            List<Student> list = new List<Student>();
+
+            DateTime dt = DateTime.Now;
+            StreamReader sr = null;
+            try
+            {
+                sr = new StreamReader("students_1.csv");
+                Student t;
+                while (!sr.EndOfStream)
+                {
+                    try
+                    {
+                        string[] s = sr.ReadLine().Split(';');
+                        t = new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]);
+                        list.Add(t);
+                    }
+                    catch (ArgumentNullException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    catch (ArgumentException exc)
+                    {
+                        Console.WriteLine(exc.Message);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+
+            // Количество студентов на 5-6 курсах
+            // подсчитать сколько студентов в возрасте от 18 до 20 лет на каком курсе учатся(частотный массив);
+            int sum = 0;
+            int[] ar = new int[6];
+            foreach (Student st in list)
+            {
+                if (st.GetCourse >= 5) sum++;
+                if (st.GetAge >= 18 && st.GetAge <= 20) ar[st.GetCourse-1]++;
+            }
+            SupportMethods.Pause($"Количество студентов на 5-6 курсах {sum}");
+            SupportMethods.Pause($"Распределение студентов от 18 до 20 лет по курсам:\n{Student.FrequencyArrayToString(ref ar, "курс", "студентов")}");
+
+            Console.WindowWidth = 120;
+            Console.BufferWidth = 120;
+
+            // в) отсортировать список по возрасту студента;
+            // Сортируем по возрасту и выводим
+            Console.WriteLine("Список, отсортированный по возрасту:");
+            list.Sort(CompareByAge);
+            foreach (Student st in list) Console.WriteLine(st.ToString());
+            SupportMethods.Pause();
+
+            // г) *отсортировать список по курсу и возрасту студента.
+            // Сортируем по курсу и возрасту и выводим
+            Console.WriteLine("Список, отсортированный по курсу и затем по возрасту:");
+            list.Sort(CompareByCourseAge);
+            foreach (Student st in list) Console.WriteLine(st.ToString());
+            SupportMethods.Pause();
+        }
+
+        /// <summary>
+        /// Method Compare by Course
+        /// </summary>
+        /// <param name="st1">первый студент</param>
+        /// <param name="st2">второй студент</param>
+        /// <returns></returns>
+        static int CompareByCource(Student st1, Student st2)
+        {
+            if (st1.GetCourse > st2.GetCourse) return 1;
+            if (st1.GetCourse < st2.GetCourse) return -1;
+            return 0;
+        }
+
+        /// <summary>
+        /// Method Compare by Age
+        /// </summary>
+        /// <param name="st1">первый студент</param>
+        /// <param name="st2">второй студент</param>
+        /// <returns></returns>
+        static int CompareByAge(Student st1, Student st2)
+        {
+            if (st1.GetAge > st2.GetAge) return 1;
+            if (st1.GetAge < st2.GetAge) return -1;
+            return 0;
+        }
+
+        /// <summary>
+        /// Method Compare by LastName then FirstName
+        /// </summary>
+        /// <param name="st1">первый студент</param>
+        /// <param name="st2">второй студент</param>
+        /// <returns></returns>
+        static int CompareByName(Student st1, Student st2)
+        {
+            //Сравниваем две фамилии
+            int res = String.Compare(st1.GetLastName, st2.GetLastName);
+
+            //Если фамилии одинаковы, то сравниваем имена
+            if (res == 0) return String.Compare(st1.GetFirstName, st2.GetFirstName); else return res;
+        }
+
+        /// <summary>
+        /// Method Compare by Course & Age
+        /// </summary>
+        /// <param name="st1">первый студент</param>
+        /// <param name="st2">второй студент</param>
+        /// <returns></returns>
+        static int CompareByCourseAge(Student st1, Student st2)
+        {
+            int res = st1.GetCourse.CompareTo(st2.GetCourse);
+            if (res == 0) return st1.GetAge.CompareTo(st2.GetAge);
+            else return res;
+        }
+
+        static void Task4()
+        {
+
+        }
+
+        static void Task5()
+        {
+
+        }
+
+        static void Task6()
+        {
+
+        }
+    }
+}
---
+++ HomeWork6/Student.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Максим Торопов
+/// Ярославль
+/// Домашняя работа 6 урока
+/// </summary>
+namespace HomeWork6
+{
+    /// <summary>
+    /// Класс Студент
+    /// 
+    /// Класс для 3 задания
+    /// </summary>
+    class Student
+    {
+        /// <summary>
+        /// Имя
+        /// </summary>
+        string firstName;
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        string lastName;
+
+        /// <summary>
+        /// Университет
+        /// </summary>
+        string univercity;
+
+        /// <summary>
+        /// Факультет
+        /// </summary>
+        string faculty;
+
+        /// <summary>
+        /// Отдел
+        /// </summary>
+        string department;
+
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        int age;
+
+        /// <summary>
+        /// Курс
+        /// </summary>
+        int course;
+
+        /// <summary>
+        /// Группа
+        /// </summary>
+        public int group;
+
+        /// <summary>
+        /// Город
+        /// </summary>
+        public string city;
+
+        /// <summary>
+        /// Свойство возвращает Имя
+        /// </summary>
+        public string GetFirstName
+        {
+            get { return firstName; }
+        }
+
+        /// <summary>
+        /// Свойство возвращает Фамилию
+        /// </summary>
+        public string GetLastName
+        {
+            get { return lastName; }
+        }
+
+        /// <summary>
+        /// Свойство возвращает Университет
+        /// </summary>
+        public string GetUnivercity
+        {
+            get { return univercity; }
+        }
+
+        /// <summary>
+        /// Свойство возвращает Факультет
+        /// </summary>
+        public string GetFaculty
+        {
+            get { return faculty; }
+        }
+
+        /// <summary>
+        /// Свойство возвращает Отдел
+        /// </summary>
+        public string GetDepartment
+        {
+            get { return department; }
+        }
+
+        /// <summary>
+        /// Свойство возвращает Возраст
+        /// </summary>
+        public int GetAge
+        {
+            get { return age; }
+        }
+
+        /// <summary>
+        /// Свойство возвращает Курс
+        /// </summary>
+        public int GetCourse
+        {
+            get { return course; }
+        }
+
+        /// <summary>
+        /// Свойство возвращает Группу
+        /// </summary>
+        public int GetGroup
+        {
+            get { return group; }
+        }
+
+        /// <summary>
+        /// Свойство возвращает Город
+        /// </summary>
+        public string GetCity
+        {
+            get { return city; }
+        }
+
+        /// <summary>
+        /// Create Student record
+        /// </summary>
+        /// <param name="firstName">Имя</param>
+        /// <param name="lastName">Фамилия</param>
+        /// <param name="univercity">Университет</param>
+        /// <param name="faculty">Факультет</param>
+        /// <param name="department">Отдел</param>
+        /// <param name="age">Возраст</param>
+        /// <param name="course">Курс</param>
+        /// <param name="group">Группа</param>
+        /// <param name="city">Город</param>
+        public Student(string firstName, string lastName, string univercity, string faculty, string department, int age, int course, int group, string city)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;            
+            this.univercity = univercity;
+            this.faculty = faculty;
+            this.department = department;
+            this.age = age;
+            this.course = course;
+            this.group = group;
+            this.city = city;
+        }
+
+        /// <summary>
+        /// Перегружаем ToString
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return String.Format($"{firstName,25}{lastName,25}{age,3}{course,2}{group,2}{city,15}");
+        }
+
+        /// <summary>
+        /// Method Fr array to String
+        /// </summary>
+        /// <param name="ar">array</param>
+        /// <param name="message1">message before number of element in array</param>
+        /// <param name="message2">message before value of element in array</param>
+        /// <returns></returns>
+        public static string FrequencyArrayToString(ref int[] ar, string message1, string message2)
+        {
+            string str = String.Empty;
+            for (int i = 0; i < ar.Length; i++) str = $"{str} {message1}:{i+1} {message2}:{ar[i]}\n";
+            return str;
+        }
+    }
+}
